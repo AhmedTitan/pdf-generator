a08f686474ce409d351e8c50f9806f1426f06bc6
Ahmed Razick <ahmedtitan@gmail.com>
2023-06-04 13:48:43 +0530

    pdf generator error added

--- src/pdf.js
+++ src/pdf.js
@@ -4,23 +4,22 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 
 export const generatePDF = async (data, template, fileName) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const compiledTemplate = handlebars.compile(template);
-      const html = compiledTemplate(data);
-      const s3Key = `${fileName}.pdf`;
+  try {
+    const compiledTemplate = handlebars.compile(template);
+    const html = compiledTemplate(data);
+    const s3Key = `${fileName}.pdf`;
 
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.setContent(html);
-      await page.pdf({ path: s3Key, format: "A4" });
-      await browser.close();
-      const buffer = fs.readFileSync(s3Key);
-      const file = await s3.uploadFile(s3Key, buffer);
-      fs.unlinkSync(s3Key);
-      return resolve(file);
-    } catch (error) {
-      return reject(error);
-    }
-  });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+    await page.pdf({ path: s3Key, format: "A4" });
+    await browser.close();
+    const buffer = fs.readFileSync(s3Key);
+    const file = await s3.uploadFile(s3Key, buffer);
+    fs.unlinkSync(s3Key);
+    return Promise.resolve(file);
+  } catch (error) {
+    await sendDCMessage(`PDF_GENERATOR_ERROR: ${JSON.stringify({ error })}`);
+    return Promise.reject(error);
+  }
 };
