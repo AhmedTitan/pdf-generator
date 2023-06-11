f96944016c2bc1d98e65da10db1353e96300ba55
Ahmed Razick <ahmedtitan@gmail.com>
2023-06-11 15:12:35 +0530

    package version change

--- src/pdf.js
+++ src/pdf.js
@@ -10,9 +10,12 @@ import stealthPlugin from "puppeteer-extra-plugin-stealth";
 export const generatePDF = async (data, template, fileName) => {
   try {
     const compiledTemplate = handlebars.compile(template);
+    console.log({ message: "template compiled" });
     const html = compiledTemplate(data);
     const s3Key = `${fileName}.pdf`;
+    console.log({ s3Key });
     puppeteerExtra.use(stealthPlugin());
+    console.log({ message: "stealth plugin added" });
     const browser = await puppeteerExtra.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
@@ -20,12 +23,19 @@ export const generatePDF = async (data, template, fileName) => {
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     });
+    console.log({ message: "browser created" });
     const page = await browser.newPage();
+    console.log({ message: "page created" });
     await page.setContent(html);
+    console.log({ message: "page content added" });
     await page.pdf({ path: s3Key, format: "A4" });
+    console.log({ message: "page format set to a4" });
     await browser.close();
+    console.log({ message: "browser closed" });
     const buffer = fs.readFileSync(s3Key);
+    console.log({ message: "file buffer" });
     const file = await s3.uploadFile(s3Key, buffer);
+    console.log({ message: "file uploaded" });
     fs.unlinkSync(s3Key);
     return Promise.resolve(file);
   } catch (error) {
