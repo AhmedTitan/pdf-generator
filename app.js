294e9f022ba7a5cfd3d274b6d954daeac35da0a1
Ahmed Razick <ahmedtitan@gmail.com>
2023-06-11 17:57:43 +0530

    console log added

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morgan("dev"));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 500000 })
+);
 
 //All available routes
 app.use("/api", indexRouter);
