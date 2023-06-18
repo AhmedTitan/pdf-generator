4fe1db7a160380dd8bcc9dc49bcc10cb4c77efe3
Ahmed Razick <ahmedtitan@gmail.com>
2023-06-18 23:18:26 +0530

    missing util function added

--- src/pdf.js
+++ src/pdf.js
@@ -46,3 +46,21 @@ const fetchAndConvertImages = async (assetImages) => {
   const bufferImages = await Promise.all(bufferImagesPromises);
   return bufferImages.filter(Boolean);
 };
+
+const isEmpty = (value) => {
+  if (value === null) {
+    return true;
+  } else if (typeof value !== "number" && value === "") {
+    return true;
+  } else if (value === "undefined" || value === undefined) {
+    return true;
+  } else if (
+    value !== null &&
+    typeof value === "object" &&
+    !Object.keys(value).length
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
