76d00d25c40f253ddca259a85863d69f883b99a9
Ahmed Razick <ahmedtitan@gmail.com>
2023-08-02 23:04:53 +0530

    s3 key error fixed

--- src/pdf.js
+++ src/pdf.js
@@ -46,7 +46,7 @@ const fetchAndConvertImages = async (assetImages) => {
       const buffer = Buffer.from(data.Body);
       return {
         imageKey: (
-          await sharp(data.Body).jpeg({ mozjpeg: true }).toBuffer()
+          await sharp(data.Body).jpeg({ mozjpeg: true, quality: 60 }).toBuffer()
         ).toString("base64"),
         label: image.label,
       };
